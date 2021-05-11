08b64710c4d0d457ac0a069c54b8e91e47cd3dc6
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-05-11 17:10:32 +0200

    set frame for future work after DL course

--- src/main.py
+++ src/main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+import os.path
+import numpy as np
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+def read_image(image):
+    img = cv2.imread(image, cv2.IMREAD_COLOR)
+    # img = np.expand_dims(img, axis=-1)
+    return img
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Set relative directories
+    project_dir = os.path.split(os.getcwd())[0]
+    res_dir = os.path.join(project_dir, 'res')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print ("test")
+    img_src = "img"
+    if img_src == "img":
+        img_dir = os.path.join(res_dir, 'test_ok.jpg')
+        cap = read_image(img_dir)
+    else:
+        cap = cv2.VideoCapture(0)
+        # Display the resulting frame or image
+        while (True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # cv2.imshow('frame', gray)
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
+
+    gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
+
+    # semantic hand segmentation
+
+    # gesture classification
+
+    # software reaction
+
+
+
+    print("test")
+
+
