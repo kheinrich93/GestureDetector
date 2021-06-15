390c7d521f91802c171d9c1034afc3f8a16b780f
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-06-15 16:15:17 +0200

    feat: added dir_dicts

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,17 @@ import os.path
 def get_dirs():
     project = os.getcwd()
     res = os.path.join(project, 'res')
+
+    testing = os.path.join(res, 'testing')
+    asl_te = os.path.join(testing, 'asl')
+    my_data_te = os.path.join(testing, 'my_data')
+    training = os.path.join(res, 'training')
+    asl_tr = os.path.join(training, 'asl')
+    my_data_tr = os.path.join(training, 'my_data')
+
     src = os.path.join(project, 'src')
 
-    dir_dict = {'project': project, 'res': res, 'src': src}
+    dir_dict = {'project': project, 'res': res,
+                'my_data_te': my_data_te, 'src': src}
 
     return dir_dict
