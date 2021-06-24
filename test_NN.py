262f416fc6588463d2fab442d22111faab68fb25
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-06-24 18:20:39 +0200

    feat: implemented test of image

--- test_NN.py
+++ test_NN.py
@@ -1,30 +1,41 @@
 import tensorflow as tf
+from tensorflow.python.ops.gen_array_ops import split
+from tensorflow.python.ops.numpy_ops.np_array_ops import imag
 import graph
 import src.tf_utils as tf_utils
+from keras.models import load_model
+import numpy as np
+from src.tf_utils import decode_img
+import os
 
 
-def te_gesture_NN(dir, batch_size):
-    BATCH_SIZE = batch_size
+def te_gesture_NN(dir):
+    n_classes = 29
 
-    test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1./255)
+    # test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    #     rescale=1./255)
 
-    test_generator = test_gen.flow_from_directory(
-        dir['asl_te'],
-        color_mode="rgb",
-        class_mode='categorical')
+    # test_generator = test_gen.flow_from_directory(
+    #     dir['asl_te'],
+    #     color_mode="rgb",
+    #     class_mode='categorical')
 
-    model = graph.create_model()
+    img_path = dir['asl_te']+'/A/A_test.jpg'
 
-    model.compile(
-        optimizer="adam",
-        loss=tf.losses.CategoricalCrossentropy(),
-        metrics=['accuracy'])
+    labels = [name for name in os.listdir(
+        dir['asl_te']) if os.path.isdir(dir['asl_te'])]
 
-    # Loads the weights
-    model.load_weights(dir['cp_gesture'])
+    img = decode_img(img_path)/255.0
 
-    # Re-evaluate the model
-    loss, acc = model.evaluate(
-        test_generator, batch_size=BATCH_SIZE, verbose=2)
-    print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+    samples = img
+
+    model = graph.Net().get_model(samples.shape, n_classes)
+
+    # model = load_model(dir['cp_gesture'])
+
+    # correct input size, adapt graph for testing
+    samples = tf.expand_dims(samples, axis=0)
+
+    predictions = model.predict(samples)
+    classes = tf.math.argmax(predictions, axis=1)
+    print('Predicted letter %s' % labels[classes[0]])
