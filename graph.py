b96f94e37352b0c238f23aa1a05cfb8eb8224666
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-06-24 18:20:13 +0200

    feat: added func model

--- graph.py
+++ graph.py
@@ -1,9 +1,45 @@
 import tensorflow as tf
-from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization
-from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras import Sequential
+from tensorflow.keras.models import Model
+# make class of net https://www.tensorflow.org/guide/checkpoint#loading_mechanics
 
 
-def conv2D_block(model, filters, kernel_regularizer):
+class Net:
+    @staticmethod
+    def conv2D_block(x, filters, kernel_regularizer, dropout=0, kernel_size=5, name=''):
+        x = Conv2D(filters, kernel_size=kernel_size, kernel_regularizer=kernel_regularizer, activation=None,
+                   padding='same', name=name)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Dropout(dropout)(x)
+        return x
+
+    def get_model(self, img_dim, n_classes):
+        filters = 32
+
+        kernel_regularizer = tf.keras.regularizers.L1()
+
+        inputs = Input(shape=(img_dim[0], img_dim[1], 3), name='input')
+
+        conv1 = self.conv2D_block(
+            inputs, filters, kernel_regularizer, dropout=0, kernel_size=5, name='CONV1')
+        pool1 = MaxPooling2D(pool_size=(4, 4))(conv1)
+
+        conv2 = self.conv2D_block(
+            pool1, filters*2, kernel_regularizer, dropout=0.5, kernel_size=5, name='CONV2')
+        pool2 = MaxPooling2D(pool_size=(4, 4))(conv2)
+
+        conv3 = self.conv2D_block(
+            pool2, filters*4, kernel_regularizer, dropout=0.5, kernel_size=5, name='CONV3')
+
+        flatten = Flatten()(conv3)
+        predictions = Dense(n_classes, activation='softmax')(flatten)
+
+        return Model(inputs=inputs, outputs=predictions)
+
+
+def seq_conv2D_block(model, filters, kernel_regularizer):
     model.add(Conv2D(filters=filters, kernel_size=5,
                      padding='same', activation='relu', kernel_regularizer=kernel_regularizer))
     model.add(BatchNormalization())
@@ -11,28 +47,31 @@ def conv2D_block(model, filters, kernel_regularizer):
     return model
 
 
-def create_model(img_dim, n_classes):
+def seq_create_model(img_dim, n_classes):
 
     kernel_regularizer = tf.keras.regularizers.L1()
 
     filters = 32
 
-    model = keras.Sequential()
+    model = Sequential()
 
-    model.add(keras.layers.InputLayer(input_shape=(img_dim[0], img_dim[1], 3)))
+    model.add(tf.keras.layers.InputLayer(
+        input_shape=(img_dim[0], img_dim[1], 3)))
 
-    model = conv2D_block(model, filters, kernel_regularizer)
+    model = seq_conv2D_block(model, filters, kernel_regularizer)
     model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Dropout(0.2))
+    model.add(BatchNormalization())
 
-    model = conv2D_block(model, filters*2, kernel_regularizer)
+    model = seq_conv2D_block(model, filters*2, kernel_regularizer)
     model.add(Dropout(0.2))
+    model.add(BatchNormalization())
 
-    model = conv2D_block(model, filters*2, kernel_regularizer)
+    model = seq_conv2D_block(model, filters*2, kernel_regularizer)
     model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Dropout(0.5))
 
-    model = conv2D_block(model, filters*4, kernel_regularizer)
+    model = seq_conv2D_block(model, filters*4, kernel_regularizer)
     model.add(Dropout(0.5))
 
     model.add(Flatten())
@@ -51,7 +90,7 @@ def create_exp_model(hp):
 
     hp_filters = hp.Int('filter', min_value=8, max_value=128, step=8)
 
-    model = keras.Sequential()
+    model = Sequential()
 
     model.add(Conv2D(filters=hp_filters, kernel_size=5, padding='same',
                      activation='relu'))
