bf510b02a75e0472c7c77325bba8668ad1553973
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-06-27 14:55:19 +0200

    feat: added hyperparam support

--- train_NN.py
+++ train_NN.py
@@ -5,36 +5,38 @@ import src.tf_utils as tf_utils
 import os
 
 
-def tr_gesture_NN(dir, batch_size, use_pretrained_cp=False, save_cp=True):
-    # params
-    n_classes = 29
-    img_dim = (64, 64)
-    BATCH_SIZE = batch_size
-    VAL_SPLIT = 0.2
-    EPOCHS = 4
+def tr_gesture_NN(dir, hp, use_pretrained_cp=False, save_cp=True):
+    # hyperparams
+    N_CLASSES = hp.n_classes
+    IMG_DIM = hp.input_dim
+    BATCH_SIZE = hp.batch_size
+    VAL_SPLIT = hp.val_split
+    EPOCHS = hp.epochs
+    SCALE_FACTOR = hp.scale_factor
 
+    # Load images to flow
     datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1./255, validation_split=VAL_SPLIT)
+        rescale=1./SCALE_FACTOR, validation_split=VAL_SPLIT)
 
     train_generator = tf_utils.create_generator_flow_from_dir(
-        dir['asl_tr'], datagen, target_size=img_dim, subset="training", color_mode="rgb", batch_size=BATCH_SIZE, shuffle=True, class_mode='categorical')
+        dir['asl_tr'], datagen, target_size=IMG_DIM, subset="training", color_mode="rgb", batch_size=BATCH_SIZE, shuffle=True, class_mode='categorical')
 
     val_generator = tf_utils.create_generator_flow_from_dir(
-        dir['asl_tr'], datagen, target_size=img_dim, subset="validation", color_mode="rgb", batch_size=BATCH_SIZE, shuffle=True, class_mode='categorical')
+        dir['asl_tr'], datagen, target_size=IMG_DIM, subset="validation", color_mode="rgb", batch_size=BATCH_SIZE, shuffle=True, class_mode='categorical')
 
-    # create and compiles model
-    #model = graph.create_model(img_dim, n_classes)
+    # Create and compile model
+    model = graph.Net().get_model(IMG_DIM, N_CLASSES)
 
-    model = graph.Net().get_model_exp(img_dim, n_classes)
-
-    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+    opt = tf.keras.optimizers.Adam()
     model.compile(
         optimizer=opt,
         loss=tf.losses.CategoricalCrossentropy(),
         metrics=['accuracy'])
 
+    # Output summary as txt in /summary
     tf_utils.summary_to_file(dir['summary'], model)
 
+    # Train network, can start with cp-weights
     if save_cp:
         cp_callback, cp_dir = tf_utils.create_cp(dir['cp_gesture'])
 
