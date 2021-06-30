44b6355637e9204e0e60e49686958989b5d44eeb
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-06-30 16:25:55 +0200

    fix: changed crop extraction

--- main_CAM.py
+++ main_CAM.py
@@ -2,9 +2,10 @@ from cv2 import imshow, waitKey, imwrite
 
 from src.img_utils import read_image, draw_bb
 from src.utils import get_dirs
-from src.tf_utils import crop_to_bb, decode_img
+from src.tf_utils import crop_to_bb, decode_img, prepare_img_for_predict
 from predict import predict_from_image
 from hp.hyperparams import hyperparams
+
 
 import tensorflow as tf
 
@@ -17,7 +18,7 @@ hp = hyperparams()
 img_src = 'img'
 
 if img_src == 'img':
-    path = dirs['my_data_te']+'/c_test.jpg'
+    path = dirs['my_data_te']+'/v_test.jpg'
     img = read_image(path)
 
 
@@ -25,12 +26,24 @@ if img_src == 'img':
 box_len = 450
 start_point = (10, 200)
 
+
 # convert to tf image
-img_cropped = crop_to_bb(
-    img, start_point[1], start_point[0], box_len, box_len)
-img_cropped = tf.image.resize(img_cropped, (200, 200))
+
+img_cropped = img[start_point[1]:start_point[1] +
+                  box_len, start_point[0]:start_point[0]+box_len]
+
+# imshow("Display window", img_cropped)
+# k = waitKey(0)
+
+
+# img_cropped = crop_to_bb(
+#     img, start_point[1], start_point[0], box_len, box_len)
+#img_cropped = tf.image.resize(img_cropped, (200, 200)).numpy()
 
 #imwrite(dirs['res']+'/b_crop.jpg', img_cropped.numpy())
+
+# imshow("Display window", img_cropped)
+# k = waitKey(0)
 
 # predict_from_image
 predict_from_image(img_cropped, dirs, hp)
@@ -38,5 +51,6 @@ predict_from_image(img_cropped, dirs, hp)
 # plot crop
 img_bb = draw_bb(img, start_point,
                  (start_point[0]+box_len, start_point[1]+box_len))
+
 # imshow("Display window", img_bb)
 # k = waitKey(0)
