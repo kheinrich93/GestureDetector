e537cdcd6001869974fcdf527db10c93f0061544
Konstantin Heinrich <konstantin.heinrich@yahoo.com>
2021-07-08 15:01:23 +0200

    feat: added letters to hp

--- hp/hyperparams.py
+++ hp/hyperparams.py
@@ -2,12 +2,15 @@ class hyperparams:
     def __init__(self):
         self.epochs = 8
         self.batch_size = 64
-        self.input_dim = (80, 80)
+        self.input_dim = (28, 28)
         self.n_classes = 7
         self.val_split = 0.2
         self.scale_factor = 255.0
-        self.color_mode = 'rgb'  # grayscale or rgb
+        self.color_mode = 'grayscale'  # grayscale or rgb
 
-        self.train_network = False
-        self.predict = True
+        self.train_network = True
+        self.predict = False
         self.tune_hp = False
+
+        # considered letters for mnist
+        self.letters = ['A', 'B', 'C', 'H', 'K', 'L', 'O']
